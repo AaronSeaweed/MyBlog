0bde496ef1acb78ea82879e4055ad74a159f6f10
Aaron <646264198@qq.com>
2019-03-26 16:14:28 +0800

    地址增删改

--- server/routes/weixin.js
+++ server/routes/weixin.js
@@ -58,11 +58,37 @@ router.post("/getfood",function(req,res,next){
  */
 router.post("/getuserinfo",function(req,res,next){
     let type=req.body.type;
+    let addid=req.body.addid;
     var selsql="select *from useraddress where usestatus = 1";
     if(type){
         selsql="select *from useraddress";
+    }else if(addid){
+        selsql="select *from useraddress where id="+addid;
     }
-    
+    db.query(selsql,function(error,rows){
+        if (error) {
+            var result = {
+                "status": "500",
+                "message": "服务器错误"
+            }
+            return res.jsonp(result);
+        }
+        else{
+            var result = {
+                "status": "200",
+                "message": "success",
+                data:rows
+            }
+            return res.jsonp(result);
+        }
+    });
+});
+
+/**
+ * 设置收货人信息
+ */
+router.post("/setuserinfo",function(req,res,next){
+    var selsql="UPDATE useraddress set usestatus=0 where usestatus = 1";
     db.query(selsql,function(error,rows){
         if (error) {
             var result = {
@@ -85,56 +111,73 @@ router.post("/getuserinfo",function(req,res,next){
 /**
  * 修改收货人信息
  */
-router.post("/setuserinfo",function(req,res,next){
-    var selsql="UPDATE useraddress set usestatus=0 where usestatus = 1";
-    let promise = new Promise((resolve, reject) =>{
-        db.query(selsql,function(error,rows){
-            if (error) {
-                var result = {
-                    "status": "500",
-                    "message": "服务器错误"
-                }
-                reject(res.jsonp(result));
+router.post("/updateuserinfo",function(req,res,next){
+    var selsql="UPDATE useraddress set username='"+req.body.username+"',Telphone='"+req.body.Telphone+"',province='"+req.body.province+"',city='"+req.body.city+"',Area='"+req.body.Area+"',detailaddress='"+req.body.detailaddress+"',usestatus="+req.body.usestatus+" where id = "+req.body.id+"";
+    db.query(selsql,function(error,rows){
+        if (error) {
+            var result = {
+                "status": "500",
+                "message": "服务器错误"
             }
-            else{
-                var result = {
-                    "status": "200",
-                    "message": "success",
-                    data:rows
-                }
-                resolve(req)
+            return res.jsonp(result);
+        }
+        else{
+            var result = {
+                "status": "200",
+                "message": "success",
+                data:rows
             }
-        });
+            return res.jsonp(result);
+        }
     });
-    promise.then(function(req) {
-        let username=req.body.username;
-        let Telphone=req.body.Telphone;
-        let province=req.body.province;
-        let city=req.body.city;
-        let Area=req.body.Area;
-        let detailaddress=req.body.detailaddress;
-        let usestatus=req.body.usestatus;
-        let id=req.body.id;
-        var selsql="UPDATE useraddress set username='"+username+"',Telphone='"+Telphone+"',province='"+province+"',city='"+city+"',Area='"+Area+"',detailaddress='"+detailaddress+"',usestatus="+usestatus+" where id = "+id+"";
-        db.query(selsql,function(error,rows){
-            if (error) {
-                var result = {
-                    "status": "500",
-                    "message": "服务器错误"
-                }
-                return res.jsonp(result);
+});
+
+
+/**
+ * 新增收货人信息
+ */
+router.post("/adduserinfo",function(req,res,next){
+    var selsql="INSERT into useraddress(username,Telphone,province,city,Area,detailaddress,addresstype,doorplate,usestatus) values ('"+req.body.username+"','"+req.body.Telphone+"','"+req.body.province+"','"+req.body.city+"','"+req.body.Area+"','"+req.body.detailaddress+"','"+req.body.addresstype+"','"+req.body.doorplate+"',0)";
+    db.query(selsql,function(error,rows){
+        if (error) {
+            var result = {
+                "status": "500",
+                "message": "服务器错误"
             }
-            else{
-                var result = {
-                    "status": "200",
-                    "message": "success",
-                    data:rows
-                }
-                return res.jsonp(result);
+            return res.jsonp(result);
+        }
+        else{
+            var result = {
+                "status": "200",
+                "message": "success",
+                data:rows
             }
-        });
-    },function(){
-        return res.jsonp(req);
+            return res.jsonp(result);
+        }
+    });
+});
+
+/**
+ * 删除收货人信息
+ */
+router.post("/deluserinfo",function(req,res,next){
+    var selsql="DELETE from useraddress where id = "+req.body.id;
+    db.query(selsql,function(error,rows){
+        if (error) {
+            var result = {
+                "status": "500",
+                "message": "服务器错误"
+            }
+            return res.jsonp(result);
+        }
+        else{
+            var result = {
+                "status": "200",
+                "message": "success",
+                data:rows
+            }
+            return res.jsonp(result);
+        }
     });
 });
 module.exports = router
