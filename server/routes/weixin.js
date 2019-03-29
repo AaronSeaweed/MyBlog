2c1b836d54c76bd4335bffc07d6a1dd823079f9c
Aaron <646264198@qq.com>
2019-03-29 15:08:28 +0800

    订单列表，提取公共组件

--- server/routes/weixin.js
+++ server/routes/weixin.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var router = express.Router();
 var db = require("../config/db");
-
+var common = require("./common");
 
 /**
  * 获取全部食物类型
@@ -180,4 +180,71 @@ router.post("/deluserinfo",function(req,res,next){
         }
     });
 });
+
+/**
+ * 获取所有订单
+ */
+router.post("/getOrder",function(req,res,next){
+//     var selsql=`select 
+// a.orderid,a.ordernum,a.goodtolprice,a.payprice,a.deliveryprice,a.Reciinfo,a.orderdate,a.orderstatus,
+// d.id as addid,d.username,d.Telphone,d.province,d.city,d.Area,d.detailaddress,
+// b.orderid as food_orderid,b.foodid,b.count,
+// c.foodid as foodinfoid,c.foodname,c.foodtypeid,c.price,c.img,c.content,
+// e.statusid,e.statusname
+// from orderlist as a 
+// INNER join useraddress as d on a.Reciinfo=d.id 
+// left join order_goodlist as b on a.orderid=b.orderid 
+// left join food as c on b.foodid=c.foodid 
+// left join orderstatus as e on a.orderstatus=e.statusid
+// `;
+var selsql=`select a.orderid,a.payprice,a.orderdate,s.statusname,f.img from orderlist as a
+left join order_goodlist as c on a.orderid=c.orderid
+left join orderstatus as s on s.statusid=a.orderstatus
+left join food as f on f.foodid = c.foodid`
+    db.query(selsql,function(error,rows){
+        if (error) {
+            var result = {
+                "status": "500",
+                "message": "服务器错误"
+            }
+            return res.jsonp(result);
+        }
+        else{
+            for(var i = 0;i<rows.length;i++){
+                rows[i].orderdate=new Date(+rows[i].orderdate+8*3600*1000).toISOString().replace(/T/g,' ').replace(/\.[\d]{3}Z/,'')
+            }
+            var orarray=[];
+            var ss=0;
+            for(var i in rows){
+                if(orarray.length>0){
+                    for(var j=ss;j<orarray.length;j++){
+                        if(orarray[j].orderid==rows[i].orderid){
+                            if(!(orarray[j].img instanceof Array)){
+                                var str=orarray[j].img
+                                orarray[j].img=[]
+                                orarray[j].img.push(str)
+                                orarray[j].img.push(rows[i].img)
+                            }else{
+                                orarray[j].img.push(rows[i].img)
+                            }
+                        }else{
+                            orarray.push(rows[i])
+                            ss=orarray.length-1
+                            break;
+                        }
+                    }
+                }else{
+                    orarray.push(rows[i])
+                }
+            }
+            var result = {
+                "status": "200",
+                "message": "success",
+                data:orarray
+            }
+            return res.jsonp(result);
+        }
+    });
+});
+
 module.exports = router
